5bc265b94a827693fac43cc1456002a069556f9f
stv <stv@stanford.edu>
2015-10-20 01:07:02 -0400

    squash

--- geoinfo/tests/test_pep8.py
+++ geoinfo/tests/test_pep8.py
@@ -9,7 +9,6 @@ class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide()
-        os.path.join
         root = os.path.join(__file__, os.pardir)
         path = os.path.abspath(os.path.sep.join([
             __file__,
