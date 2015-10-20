37f6a4342f02ddf72513520d8b58b27b1042b27b
stv <stv@stanford.edu>
2015-10-20 00:54:38 -0700

    cleanup

--- geoinfo/settings.py
+++ geoinfo/settings.py
@@ -1,25 +1,23 @@
+# coding: utf-8
 import os
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    },
-}
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django_nose',
-)
-
-MIDDLEWARE_CLASSES = (
-    'geoinfo',
-)
 
 PROJECT_ROOT = os.path.abspath(os.path.join(__file__, os.pardir))
 DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'geoip')
 GEOIP_PATH = os.path.join(DATA_PATH, 'GeoIP.dat')
 GEOIPV6_PATH = os.path.join(DATA_PATH, 'GeoIPv6.dat')
-
+MIDDLEWARE_CLASSES = (
+    'geoinfo',
+)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': 'intentionally-omitted',
+    },
+}
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django_nose',
+)
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
