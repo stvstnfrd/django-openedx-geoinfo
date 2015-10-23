48414c6f642f7da1d50130c3ec7197832173d4bf
stv <stv@stanford.edu>
2015-10-22 22:49:08 -0700

    Clean up manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-"""Manage.py file for XBlock"""
+"""
+Manage.py file for XBlock
+"""
 import os
 import sys
+
+from django.core.management import execute_from_command_line
 
 PROJECT_ROOT = os.path.dirname(__file__)
 
 if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoinfo.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoinfo.settings')
     sys.path.insert(0, PROJECT_ROOT)
-
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
