5b280097e08772ff272857964970579094bfaf72
stv <stv@stanford.edu>
2015-10-30 18:12:41 -0700

    Try Tox

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 from setuptools import setup
+from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
 
 _version = '1.0.0'
 with open('README.markdown') as file_input:
     readme = file_input.read()
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
 
 setup(
     # Metadata
@@ -22,16 +45,11 @@ setup(
         'Programming Language :: Python :: 2.7'
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
     ],
-
     # URLs
     url='https://github.com/stvstnfrd/django-openedx-geoinfo',
     download_url='https://github.com/stvstnfrd/django-openedx-geoinfo/tarball/release/version/' + _version,
-
     # Packages
-    packages=[
-        'geoinfo',
-        'geoinfo.tests',
-    ],
+    packages=find_packages('.'),
     install_requires=[
         'django==1.4.22',
         'django-ipware==1.1.0',
@@ -42,11 +60,13 @@ setup(
         'coverage',
         'django-nose',
         'mock==1.0.1',
-        'pep8',
-        'pyflakes',
+        'flake8',
+        'tox',
     ],
-    test_suite='geoinfo.tests.run.run_tests',
-
+    test_suite='geoinfo.test.run.run_tests',
+    cmdclass={
+        'test': Tox,
+    },
     # Distribution
     package_dir={
         'geoinfo': 'geoinfo',
